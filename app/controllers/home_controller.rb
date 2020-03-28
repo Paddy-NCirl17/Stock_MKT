ed5ee3228286db88fe9cd71d84447d7a30706956
Patrick Moran <p.jmoran@icloud.com>
2020-03-28 16:33:38 +0000

    installed devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
   def index
+  	@api =StockQuote::Stock.new(api_key:'pk_72a2fb0cbe3746f9869108bc50f2a036')
+
+  	if params[:ticker] == ""
+  		@nothing = "Please enter a symbol"
+  	elsif params[:ticker]
+		@stock = StockQuote::Stock.quote(params[:ticker])
+		if !@stock
+		    @error = "Stock symbol does not exist"
+		end	
+	end
+
   end
+
+  def about
+  end
+
 end
