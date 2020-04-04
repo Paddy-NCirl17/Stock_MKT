df54556c590c034290740692be62875a62872583
Patrick Moran <p.jmoran@icloud.com>
2020-04-04 10:10:48 +0100

    custom gem update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
-require 'stock_api_wrapper'
 class HomeController < ApplicationController
   def index
   	@api =StockQuote::Stock.new(api_key:'pk_72a2fb0cbe3746f9869108bc50f2a036')
-    @exchange = StockApiWrapper::Stock.find(150)
     if params[:ticker] == ""
         @nothing = "You forgot to enter a symbol."
     elsif params[:ticker]
